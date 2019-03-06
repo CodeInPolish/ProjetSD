92f73e454fed0d76f00bd3972584ac8f1f320ee5
CodeInPolish <marcin.krasowski@hotmail.fr>
2019-03-06 10:31:54 +0100

    Start of impl Graph

--- src/model/Graph.java
+++ src/model/Graph.java
@@ -10,13 +10,39 @@ public class Graph {
 	public Graph() {
 	}
 	
-	
-	
+	public Map<String, Actor> getActors() {
+		return actors;
+	}
+
+	public void setActors(Map<String, Actor> actors) {
+		this.actors = actors;
+	}
+
+	public Map<String, Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(Map<String, Movie> movies) {
+		this.movies = movies;
+	}
+
 	public void calculerCheminLePlusCourt(String act1, String act2, String file) {
-		
+		String result = bfs(act1, act2);
 	}
 	
 	public void calculerCheminCoutMinimum(String act1, String act2, String file) {
+		String result = dijkstra(act1, act2);
+	}
+	
+	private String bfs(String start, String finish) {
+		return "";
+	}
+	
+	private String dijkstra(String start, String finish) {
+		return "";
+	}
+	
+	private void writeFile(String data, String file) {
 		
 	}
 		
