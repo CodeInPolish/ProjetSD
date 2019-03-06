a874e96f80dd8d7954da1827792ef92a935da72a
CodeInPolish <marcin.krasowski@hotmail.fr>
2019-03-06 09:31:51 +0100

    Implemented Actor class

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -1,5 +1,64 @@
 package model;
 
 public class Actor {
+	private String firstname;
+	private String lastname;
+	
+	public Actor() {
+		
+	}
+	
+	public Actor(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+	public String getFirstname() {
+		return this.firstname;
+	}
+	
+	public String getLastname() {
+		return this.lastname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Actor other = (Actor) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		return true;
+	}
+	
+	
 }
